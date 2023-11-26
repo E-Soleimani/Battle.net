64f21efeabd9e86a2b88f96ab65d521f1e8d547f
E-Soleimani <amin.soleiimani@gmail.com>
2023-11-26 15:21:06 +0330

    Some Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}",
+  content: ["./*.{html,js}",
             "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
